f5e131ad50bfdbc63ccd935d72b57201495d4f54
Jose Ignacio Lavecchia Suarez <127800259+Joseignlave1@users.noreply.github.com>
2024-08-31 17:35:02 -0300

    Funcionalidad de crear productos por boton implementada

--- WebProductos/Components/script.js
+++ WebProductos/Components/script.js
@@ -5,172 +5,182 @@ const input = document.getElementById("searchInput");
 const cards = document.getElementsByClassName("card");
 const productList = document.getElementById("productList");
 
-//JSON de productos
-const productos = [
-    {
-      "nombre": "Coca Cola",
-      "precio": 240,
-      "descripcion": "Botella de coca cola de 3 litros, ideal para compartir en familia",
-      "imagen": "/WebProductos/ImagenesProductos/CocaColaBotella.jpg",
-      "alt": "Botella de Coca Cola",
-      "categoria": "Bebidas"
-    },
-    {
-      "nombre": "Churrascos",
-      "precio": 800,
-      "descripcion": "120g 3 Unidades de churrascos, ideal para los asados en familia",
-      "imagen": "/WebProductos/ImagenesProductos/Churrascos.jpg",
-      "alt": "120g, 3 unidades de churrascos en bolsa",
-      "categoria": "Carnes"
-    },
-    {
-      "nombre": "Dulce De Leche",
-      "precio": 1200,
-      "descripcion": "Dulce de leche La Serenisima importado Argentino",
-      "imagen": "/WebProductos/ImagenesProductos/DulceDeLeche.jpg",
-      "alt": "Dulce de leche La Serenisima importado Argentino",
-      "categoria": "Dulces"
-    },
-    {
-      "nombre": "Manzanas Rojas",
-      "precio": 89,
-      "descripcion": "Manzanas Rojas de excelente calidad",
-      "imagen": "/WebProductos/ImagenesProductos/ManzanasRojas.jpg",
-      "alt": "Manzanas Rojas",
-      "categoria": "Frutas"
-    },
-    {
-      "nombre": "Peras",
-      "precio": 100,
-      "descripcion": "Peras de excelente calidad",
-      "imagen": "/WebProductos/ImagenesProductos/Peras.jpg",
-      "alt": "Peras",
-      "categoria": "Frutas"
-    },
-    {
-      "nombre": "Sandias",
-      "precio": 200,
-      "descripcion": "Sandias enteras de excelente calidad",
-      "imagen": "/WebProductos/ImagenesProductos/Sandias.jpg",
-      "alt": "Sandias",
-      "categoria": "Frutas"
-    },
-    {
-        "nombre": "Agua Mineral",
-        "precio": 150,
-        "descripcion": "Botella de agua mineral de 2 litros, sin gas",
-        "imagen": "/WebProductos/ImagenesProductos/agua.jpg",
-        "alt": "Botella de agua mineral",
-        "categoria": "Bebidas"
-      },
-      {
-        "nombre": "Cerveza",
-        "precio": 650,
-        "descripcion": "Lata de cerveza de 1 litro, ideal para compartir en familia",
-        "imagen": "/WebProductos/ImagenesProductos/cerveza.jpg",
-        "alt": "Lata de cerveza",
-        "categoria": "Bebidas"
-      },
-      {
-        "nombre": "Vino",
-        "precio": 1300,
-        "descripcion": "Botella de vino de 1 litro, ideal para compartir en familia",
-        "imagen": "/WebProductos/ImagenesProductos/vino.jpg",
-        "alt": "Botella de vino",
-        "categoria": "Bebidas"
-      },
-      {
-        "nombre": "Mandarinas",
-        "precio": 50,
-        "descripcion": "Mandarinas de excelente calidad",
-        "imagen": "/WebProductos/ImagenesProductos/mandarina.jpg",
-        "alt": "Mandarinas",
-        "categoria": "Frutas"
-      },
-      {
-        "nombre": "Milannesa de pollo",
-        "precio": 500,
-        "descripcion": "Milanesa de pollo de 1 kilo, ideal para compartir en familia",
-        "imagen": "/WebProductos/ImagenesProductos/milanesa.jpg",
-        "alt": "Milanesa de pollo",
-        "categoria": "Carnes"
-      },
-      {
-        "nombre": "Bananas",
-        "precio": 70,
-        "descripcion": "Bananas de excelente calidad",
-        "imagen": "/WebProductos/ImagenesProductos/bananas.jpg",
-        "alt": "Bananas",
-        "categoria": "Frutas"
-      },
-      {
-        "nombre": "Limones",
-        "precio": "30$ Kilo",
-        "descripcion": "Limones de excelente calidad",
-        "imagen": "/WebProductos/ImagenesProductos/limones.jpg",
-        "alt": "Limones",
-        "categoria": "Frutas"
-      }
-  ];
-
-  //Función para crear las cartas de los productos.
-
-  const createCard = (e) => {
-    let card = "";
-
-    card += `<div class="card" >
-                    <div class="car-image">
-                        <figure class="image is-4by3">
-                            <img 
-                            src="${e.imagen}"
-                            alt=${e.alt}
-                            draggable="false"                           
-                            >
-                        </figure>
-                    </div>
-                    <div class="media-content">
-                        <p class="title is-5 titulo_producto">${e.nombre}</p>
-                        <p class="subtitle">$${e.precio}</p>
-                    </div>
-                    <div class="card-content">
-                        <div class="content">
-                            ${e.descripcion}
-                        </div>
-                    </div>
-                    <div class="card-content">
-                        <div class="content">
-                            ${e.categoria}
-                        </div>
-                    </div>
-                </div> `
-        productList.innerHTML += card;
-  };
-
 const cleanProductList = () => {
     productList.innerHTML = "";
  }
 
-  //Mostrar los productos al inicio.
-  let category = [];
+ const noProductosMessage = () => {
+  productList.innerHTML = "No se encontraron productos";
+ }
 
-    const showProducts = () => { 
-        productos.forEach((e) => {
 
-            if (!category.includes(e.categoria)) {
-                category.push(e.categoria);
-            }
+  //Cargar los productos desde el local storage
+  const loadProductosOnLocalStorage = () => {
+    const saveProducts = localStorage.getItem("products");
+    return saveProducts ? JSON.parse(saveProducts) : [];
+  }
 
-            createCard(e);
+  //Lista de productos cargados dinamicamente en el localStorage
+  let products = loadProductosOnLocalStorage();
+  
+   //Guardar los productos en el LocalStorage para que no se pierdan al hacer refresh en la pagina
+   const saveProductsOnLocalStorage = () => {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
 
-            }); 
-            
-            createDropdownItemsCategory(category);
-    }
+    //Crear producto al darle click al boton guardar
 
-document.addEventListener("DOMContentLoaded", () => {
-    showProducts();
+const button_crear_producto = document.getElementById("button_crear_producto");
+const nombre_producto = document.getElementById("input_nombre");
+const precioProducto = document.getElementById("input_precio");
+const descripcion_producto = document.getElementById("descripcion_input");
+const categoria = document.getElementById("input_categoria");
+
+const crearURLDinamica = (file, callback) => {
+  let fr  = new FileReader();
+  fr.readAsDataURL(file);
+  fr.addEventListener("load", () => {
+    callback(fr.result);
+  });
+}
+const crearProducto = () => {
+  crearURLDinamica(fileInput.files[0], (urlImagen) => {
+    const nuevoProducto = {
+      "nombre": nombre_producto.value,
+      "precio": parseInt(precioProducto.value),
+      "descripcion": descripcion_producto.value,
+      "imagen": urlImagen,
+      "categoria": categoria.value
+    };
+    products.push(nuevoProducto);
+    saveProductsOnLocalStorage();
+    createCard(nuevoProducto);
+
+    if (!category.includes(nuevoProducto.categoria)) {
+      category.push(nuevoProducto.categoria);
+      createDropdownItemsCategory(category);
+  }
+  })
+}
+
+const limpiarProductos = () => {
+  products = []; 
+  saveProductsOnLocalStorage();
+  cleanProductList(); 
+
+  category = [];
+  createDropdownItemsCategory(category); 
+};
+
+button_crear_producto.addEventListener("click", crearProducto);
+
+//Modal
+const openModalButton = document.getElementById("open_Modal_Button");
+const closeModalButton = document.getElementById("close_Modal_Button");
+const cancelModalButton = document.getElementById("cancel_Modal_Button");
+const productModal = document.getElementById("product_Modal");
+const saveModal = document.getElementById("button_crear_producto");
+openModalButton.addEventListener("click", () => {
+  productModal.classList.add("is-active")
 
 });
+
+cancelModalButton.addEventListener("click", () => {
+  productModal.classList.remove("is-active");
+});
+saveModal.addEventListener("click", () => {
+  productModal.classList.remove("is-active");
+})
+
+let category = [];
+
+
+//Función para crear las cartas de los productos.
+
+const createCard = (e) => {
+ let card = "";
+
+ card += `<div class="card" >
+                 <div class="car-image">
+                     <figure class="image is-4by3">
+                         <img 
+                         src="${e.imagen}"
+                         alt=""
+                         draggable="false"                           
+                         >
+                     </figure>
+                 </div>
+                 <div class="media-content">
+                     <p class="title is-5 titulo_producto">${e.nombre}</p>
+                     <p class="subtitle">$${e.precio}</p>
+                 </div>
+                 <div class="card-content">
+                     <div class="content">
+                         ${e.descripcion}
+                     </div>
+                 </div>
+                 <div class="card-content">
+                     <div class="content">
+                         ${e.categoria}
+                     </div>
+                 </div>
+             </div> `
+     productList.innerHTML += card;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+ products.forEach((e) => {
+   if (!category.includes(e.categoria)) {
+     category.push(e.categoria);
+ }
+
+ createCard(e);
+ }); 
+ 
+ createDropdownItemsCategory(category);
+});
+
+
+  //Imagen crear producto(upload dandole click y drag and drop)
+const fileInput = document.getElementById("image");
+const dropZone = document.getElementById("result-image");
+const img = document.getElementById("img-result");
+
+dropZone.addEventListener("click", () => fileInput.click());
+
+dropZone.addEventListener("dragover", (e) =>  {
+  e.preventDefault();
+  dropZone.classList.add("form-file__result--active");
+});
+
+dropZone.addEventListener("dragleave", (e) => {
+  e.preventDefault();
+  dropZone.classList.remove("form-file__result--active");
+});
+
+const uploadImage = (file) => {
+  const fileReader = new FileReader();
+  fileReader.readAsDataURL(file);
+
+  fileReader.addEventListener("load", (e) => {
+    img.setAttribute("src", e.target.result);
+  });
+}
+
+dropZone.addEventListener("drop", (e) =>  {
+  e.preventDefault();
+  fileInput.files = e.dataTransfer.files;
+  const file = fileInput.files[0];
+  uploadImage(file);
+  dropZone.classList.add("image-uploaded");
+});
+
+fileInput.addEventListener("change", (e) => {
+  const file = fileInput.files[0];
+  uploadImage(file);
+  dropZone.classList.add("image-uploaded");
+});
+
 
 
 //Filtrar productos según el input de búsqueda.
@@ -180,7 +190,7 @@ const filterProducts = () => {
 
         //Primero filtramos los productos que coinciden con el valor del input
         
-        const productosFiltrados = productos.filter((e) => {
+        const productosFiltrados = products.filter((e) => {
             const tituloProducto = e.nombre.toLowerCase();
             return tituloProducto.includes(valorInput);
         });
@@ -191,15 +201,15 @@ const filterProducts = () => {
                 createCard(e);
         });
         } else {
-            card = `<p> No se han encontrado productos </p> `
+            noProductosMessage();
         }
-        productList.innerHTML = card;
     };
 
 input.addEventListener("input", filterProducts);
 
 
 //Dropdown de filtros.
+
 
  const dropdown_button = document.getElementById("dropdown_button");
  const dropdownMenu = document.getElementsByClassName("dropdown")[0];
@@ -209,6 +219,7 @@ input.addEventListener("input", filterProducts);
     dropdownMenu.classList.toggle('is-active');
  });
 
+
  //Filtrado Default
  const buttonDefault = document.getElementById("boton_default");
 
@@ -216,13 +227,13 @@ input.addEventListener("input", filterProducts);
     
     cleanProductList();
 
-    productos.forEach((f) => {
-        console.log(f);
+    products.forEach((f) => {
         createCard(f);
     });
  }
  
  buttonDefault.addEventListener("click", productosFiltradosDefault);
+
  
  //Filtrado MenorPrecio
  const buttonMenorPrecio = document.getElementById("boton_menor_precio");
@@ -232,7 +243,7 @@ input.addEventListener("input", filterProducts);
     cleanProductList();
 
     //Slice para no modificar el array original.
-    const sortedProducts = productos.slice().sort((a, b) => a.precio - b.precio);
+    const sortedProducts = products.slice().sort((a, b) => a.precio - b.precio);
     
     sortedProducts.forEach((e) => {
         createCard(e);
@@ -250,7 +261,7 @@ input.addEventListener("input", filterProducts);
     cleanProductList();
 
     //Slice para no modificar el array original.
-    const sortedProducts = productos.slice().sort((a, b) => b.precio - a.precio);
+    const sortedProducts = products.slice().sort((a, b) => b.precio - a.precio);
 
     sortedProducts.forEach((e) => {
         createCard(e);
@@ -292,7 +303,7 @@ const createAction = (dropdownId) => {
     dropdownItem.addEventListener('click', () => {
         const category = dropdownItem.textContent;
         productList.innerHTML = '';
-        const filteredProducts = productos.filter(product => product.categoria === category);
+        const filteredProducts = products.filter(product => product.categoria === category);
         filteredProducts.forEach(product => {
             createCard(product);
         });
